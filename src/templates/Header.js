aa7796847a1068accd152257ec629cf47089c2c7
veronicagimenez279 <81596467+veronicagimenez279@users.noreply.github.com>
2022-09-12 15:34:04 -0300

    Create Header.js

---
+++ src/templates/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+  const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+                <h1>
+                    <a href="/"><img src="/aerolab-logo.svg" alt="Logo Aerolab"> Aerolab</a>
+                </h1>
+            </div>
+            <div class="Header-nav">
+            </div>
+        </div>   
+    `;
+  return view;
+};
+
+export default Header;
