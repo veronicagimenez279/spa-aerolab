3fd705d7fe0458daaf6d8ec80270a1fac80cc4f3
veronicagimenez279 <81596467+veronicagimenez279@users.noreply.github.com>
2022-09-12 15:34:01 -0300

    Create index.js

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+//componentes
+import Header from "../templates/Header";
+import Home from "../pages/Home";
+import Product from "../pages/Product";
+import Error404 from "../pages/Error404";
+
+//funciones
+import getHash from "../utils/getHash";
+import resolveRoutes from "../utils/getResolveRoutes";
+
+const routes = {
+  "/": Home,
+  "/:id": Product,
+  "/contact": "Contact",
+};
+
+const router = async () => {
+  const header = null || document.getElementById("header");
+  const content = null || document.getElementById("content");
+
+  header.innerHTML = await Header();
+  let hash = getHash();
+  let route = await resolveRoutes(hash);
+  let render = routes[route] ? routes[route] : Error404;
+  content.innerHTML = await render();
+};
+
+export default router;
