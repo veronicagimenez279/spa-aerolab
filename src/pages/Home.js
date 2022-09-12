17fba71450ba824b6bd0d44e67dc9d4641a0261c
veronicagimenez279 <81596467+veronicagimenez279@users.noreply.github.com>
2022-09-12 15:33:56 -0300

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import getData from "../utils/getData";
+
+const Home = async () => {
+  const products = await getData();
+
+  const view = `
+        <div class="container">
+          <img src="/header-x2.png" alt="Header Image" style="width:100%;">
+          <div class="bottom-left">Electronics</div>
+        </div>
+        <div class ="Products">
+          ${products.map((product) => `
+            <article class ="Products-item">
+            <a href ="#/${product._id}/">
+                <img src="${product.img.url}" alt="${product.name}">
+                <div class ="ProductCategory">${product.category}</div>
+                <div class ="ProductName">${product.name}</div>
+                
+              </a>
+            </article>
+            `).join('')}
+          
+        </div>
+        <div class ="TotalProducts">${products.length} of ${products.length} products</div>
+
+    `
+  return view;
+}
+
+export default Home;
+
