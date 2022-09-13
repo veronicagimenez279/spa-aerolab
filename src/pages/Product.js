a241dda61fd444f2998a3da580a092d5abffc80b
veronicagimenez279 <81596467+veronicagimenez279@users.noreply.github.com>
2022-09-13 03:17:28 -0300

    Update Product.js

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -3,18 +3,19 @@ import getData from "../utils/getData";
 
 const Product = async () => {
   const id = getHash();
-  const product = getData(id);
+  const product = await getData(id);
   const view = `
         <div class="Products-inner">
-            <article class="Product-card">
-                <img src="${product.img.hdUrl}" alt="${product.name}">
+            <article class="Products-card">
+                <div><img src="${product.img.hdUrl}" alt="${product.name}">
                 <h2>${product.name}</h2>
             </article>
-            <article class="Product-card">
-                <img src="${product.img.hdUrl}" alt="${product.name}">
-                <h2>${product.name}</h2>
-            </article>
-            
+            <article class="Products-card">
+                <h3><div class="ProductCategory">Cost </div><div class="ProductName">${product.cost}</div></h3>
+                <h3><div class="ProductCategory">Category </div><div class="ProductName">${product.category}</div></h3>
+                <h3><div class="ProductCategory">ID </div><div class="ProductName">${product._id}</div></h3>
+
+            </article>    
         </div> 
     `;
   return view;
