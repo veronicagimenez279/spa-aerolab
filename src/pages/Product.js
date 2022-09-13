05e6f5cb7524b36ed8439c2b90bdc0a00a3f6036
veronicagimenez279 <81596467+veronicagimenez279@users.noreply.github.com>
2022-09-13 13:19:26 -0300

    Acomodar codigo

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -11,9 +11,14 @@ const Product = async () => {
                 <h2>${product.name}</h2>
             </article>
             <article class="Products-card">
-                <h3><div class="ProductCategory">Cost </div><div class="ProductName">${product.cost}</div></h3>
-                <h3><div class="ProductCategory">Category </div><div class="ProductName">${product.category}</div></h3>
-                <h3><div class="ProductCategory">ID </div><div class="ProductName">${product._id}</div></h3>
+                <h3>
+                    <div class="ProductCategory">Cost </div>
+                    <div class="ProductName">${product.cost}</div>
+                    <div class="ProductCategory">Category </div>
+                    <div class="ProductName">${product.category}</div>
+                    <div class="ProductCategory">ID </div>
+                    <div class="ProductName">${product._id}</div>
+                </h3>
 
             </article>    
         </div> 
