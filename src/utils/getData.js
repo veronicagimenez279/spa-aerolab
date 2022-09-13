8401e98ba16be3986390c9d91a27c4e7a2e00e4b
veronicagimenez279 <81596467+veronicagimenez279@users.noreply.github.com>
2022-09-13 03:18:16 -0300

    Agrega forma de obtener info individual de cada producto

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,11 +1,9 @@
 const API = "https://coding-challenge-api.aerolab.co/products/";
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzFhNDBmODFmMWY0NTAwMjEwOTliNTkiLCJpYXQiOjE2NjI2NjQ5NTJ9.xdiAuFNKjXVF1nKUGQ9a0Mx44lC5UfK5fb4Yt7rx8CY";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzFhNDBmODFmMWY0NTAwMjEwOTliNTkiLCJpYXQiOjE2NjI2NjQ5NTJ9.xdiAuFNKjXVF1nKUGQ9a0Mx44lC5UfK5fb4Yt7rx8CY";
 
 const getData = async (id) => {
-  const apiURL = id ? `${API}${id}` : API;
   try {
-    const response = await fetch(apiURL, {
+    const response = await fetch(API, {
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
@@ -13,8 +11,9 @@ const getData = async (id) => {
       },
     });
     const data = await response.json();
-    console.log(data);
-    return data;
+    const found = data[id];
+    console.log(id ? found : data)
+    return id ? found : data;
   } catch (error) {
     console.log("Fetch Error", error);
   }
